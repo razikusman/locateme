9ee2f9fec915b191db0ce0e471bb564ad3475264
razikusman <UsmanGit2111@gmail.com>
2022-04-19 00:57:33 +0530

    tab view swi[e screen fixed

--- app/src/main/java/com/example/locateme/DashBoard.java
+++ app/src/main/java/com/example/locateme/DashBoard.java
@@ -1,14 +1,35 @@
 package com.example.locateme;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class DashBoard extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager pager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
+
+        tabLayout = findViewById(R.id.tabLayout);
+        pager = findViewById(R.id.viewPager);
+
+        tabLayout.setupWithViewPager(pager);
+
+        VPAdapter vpAdapter = new VPAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) ;
+
+        vpAdapter.addFragment( new fragment1() , "Tab 1");
+        vpAdapter.addFragment( new fragmnt2() , "Tab 2");
+        vpAdapter.addFragment( new fragment3() , "Tab 2");
+
+        pager.setAdapter(vpAdapter);
     }
 }
